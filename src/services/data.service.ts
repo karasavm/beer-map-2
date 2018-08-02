4e372d23536eb395cf4285dc642fe31fbd8eed60
Michael Karasavvas <karasavm@gmail.com>
2018-08-02 21:17:00 +0300

    mikes server tk

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -13,8 +13,8 @@ export class DataService {
   map: any = null;
   selectedArea: any = null;
   loading: any;
-  public pinsPath: string = 'http://138.68.180.1/api/images/pins/150X150cp/';
-  public logosPath: string = 'http://138.68.180.1/api/images/pins/compressed/';
+  public pinsPath: string = 'https://mikesdevserver.tk/api/images/pins/150X150cp/';
+  public logosPath: string = 'http://mikesdevserver.tk/api/images/pins/compressed/';
 
 
 
